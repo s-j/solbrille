2d2077ad57fea2b2c2fd74d7587ab181af08cd79
janmaxim <janmaxim@4b95ddc4-ef9e-11dd-b507-8b4ecd5210be>
2009-03-24 18:12:54 +0000

    Finally made static file-serving to work!

--- trunk/src/main/com/ntnu/solbrille/frontend/FrontEnd.java
+++ trunk/src/main/com/ntnu/solbrille/frontend/FrontEnd.java
@@ -1,15 +1,16 @@
 package com.ntnu.solbrille.frontend;
 
 import org.mortbay.jetty.Server;
-import org.mortbay.jetty.handler.ResourceHandler;
-import org.mortbay.jetty.handler.ContextHandler;
-import org.mortbay.jetty.handler.ContextHandlerCollection;
-import org.mortbay.jetty.handler.HandlerList;
+import org.mortbay.jetty.Handler;
+import org.mortbay.jetty.handler.*;
 import org.mortbay.jetty.servlet.ServletHandler;
 import org.mortbay.jetty.servlet.ServletHolder;
+import org.mortbay.jetty.servlet.DefaultServlet;
+import org.mortbay.jetty.servlet.Context;
 import org.mortbay.jetty.bio.SocketConnector;
 import org.mortbay.resource.URLResource;
 import org.mortbay.resource.FileResource;
+import org.mortbay.resource.Resource;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -36,6 +37,7 @@ import com.sun.corba.se.spi.activation._ActivatorImplBase;
  * //The web frontend, based on Jetty
  *
  * @author <a href="mailto:arnebef@yahoo-inc.com">Arne Bergene Fossaa</a>
+ * @author <a href="mailto:janmaxim@gmail.com">Jan Maximilian Winther Kristiansen</a>
  * @version $Id$.
  */
 public class FrontEnd {
@@ -45,13 +47,20 @@ public class FrontEnd {
     public static void main(String args[]) throws IOException{
         Server server = new Server();
         final SearchEngineMaster master = SearchEngineMaster.createMaster();
-        
 
         SocketConnector connector = new SocketConnector();
         connector.setPort(8080);
-        server.addConnector(connector);
-        //Now we have to decide how to handle stuff
 
+        server.addConnector(connector);
+
+        // Creating ContextHandlers
+        ContextHandler servlets = new ContextHandler();
+        ContextHandler media = new ContextHandler();
+
+        servlets.setContextPath("/servlets");
+        media.setContextPath("/media");
+
+        //Now we have to decide how to handle stuff
         ServletHandler servletHandler = new ServletHandler();
         servletHandler.addServletWithMapping(new ServletHolder(new SearchServlet(master)),"/search");
         servletHandler.addServletWithMapping(new ServletHolder(new FeederServlet(master)),"/feed");
@@ -63,31 +72,41 @@ public class FrontEnd {
 
         servletHandler.addServletWithMapping(new ServletHolder(new HttpServlet() {
             protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-                StringBuilder sb = new StringBuilder();
                 Iterator<DictionaryTerm> iter = master.getDict().iterator();
                 while(iter.hasNext()) {
                     response.setContentType("text");
                     response.getOutputStream().println(iter.next().getTerm());
                 }
-
             }
         }),"/dumpdict");
 
-        server.addHandler(servletHandler);
+        servlets.setHandler(servletHandler);
 
-        // Handles static files
-        // ResourceHandler resourceHandler = new ResourceHandler();
-        // resourceHandler.setResourceBase(".");
-        // server.addHandler(resourceHandler);
+        ServletHandler mediaHandler = new ServletHandler();
+        ServletHolder sh = new ServletHolder(DefaultServlet.class);
+        sh.setInitParameter("dirAllowed", String.valueOf(true));
+        sh.setInitParameter("resourceBase", System.getProperty("user.dir"));
+        mediaHandler.addServletWithMapping(sh, "/*");
+        
+        media.setHandler(mediaHandler);
 
+        ContextHandlerCollection contexts = new ContextHandlerCollection();
+        contexts.setHandlers(new Handler[] {servlets, media});
+
+        HandlerCollection handlers = new HandlerCollection();
+        handlers.setHandlers(new Handler[]{contexts,new DefaultHandler()});
+
+        server.setHandler(handlers);
+        
         try {
             master.start();
             server.start();
+            server.join();
         } catch (Exception e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
 
-
+        System.out.println(System.getProperty("jetty.home"));
 
         Thread stopper = new Thread() {
             @Override
